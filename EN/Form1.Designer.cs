bfdb7d28f6c3972e24f0c1adbc9255bd101717f3
rakpinar <akpinarrumeysa1@gmail.com>
2024-01-09 16:42:13 +0300

    Add project files.

---
+++ EN/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace EN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxCode = new System.Windows.Forms.TextBox();
+            this.textBoxProduct = new System.Windows.Forms.TextBox();
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.textBoxMarket = new System.Windows.Forms.TextBox();
+            this.textBoxUnit = new System.Windows.Forms.TextBox();
+            this.dataGridViewInflation = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxProductSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInflation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(300, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UNIT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "CODE";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "PRODUCT";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(100, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "QUANTITY";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(300, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "PRICE";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(300, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "MARKET";
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Location = new System.Drawing.Point(171, 58);
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.Size = new System.Drawing.Size(57, 20);
+            this.textBoxCode.TabIndex = 6;
+            // 
+            // textBoxProduct
+            // 
+            this.textBoxProduct.Location = new System.Drawing.Point(171, 113);
+            this.textBoxProduct.Name = "textBoxProduct";
+            this.textBoxProduct.Size = new System.Drawing.Size(57, 20);
+            this.textBoxProduct.TabIndex = 7;
+            // 
+            // textBoxQuantity
+            // 
+            this.textBoxQuantity.Location = new System.Drawing.Point(171, 174);
+            this.textBoxQuantity.Name = "textBoxQuantity";
+            this.textBoxQuantity.Size = new System.Drawing.Size(57, 20);
+            this.textBoxQuantity.TabIndex = 8;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(365, 113);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(57, 20);
+            this.textBoxPrice.TabIndex = 9;
+            // 
+            // textBoxMarket
+            // 
+            this.textBoxMarket.Location = new System.Drawing.Point(365, 174);
+            this.textBoxMarket.Name = "textBoxMarket";
+            this.textBoxMarket.Size = new System.Drawing.Size(57, 20);
+            this.textBoxMarket.TabIndex = 10;
+            // 
+            // textBoxUnit
+            // 
+            this.textBoxUnit.Location = new System.Drawing.Point(365, 51);
+            this.textBoxUnit.Name = "textBoxUnit";
+            this.textBoxUnit.Size = new System.Drawing.Size(57, 20);
+            this.textBoxUnit.TabIndex = 11;
+            // 
+            // dataGridViewInflation
+            // 
+            this.dataGridViewInflation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewInflation.Location = new System.Drawing.Point(103, 276);
+            this.dataGridViewInflation.Name = "dataGridViewInflation";
+            this.dataGridViewInflation.Size = new System.Drawing.Size(602, 139);
+            this.dataGridViewInflation.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(635, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 27);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "GETİR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(635, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 27);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "EKLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxProductSearch
+            // 
+            this.textBoxProductSearch.Location = new System.Drawing.Point(609, 53);
+            this.textBoxProductSearch.Name = "textBoxProductSearch";
+            this.textBoxProductSearch.Size = new System.Drawing.Size(63, 20);
+            this.textBoxProductSearch.TabIndex = 15;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(687, 49);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(73, 31);
+            this.buttonSearch.TabIndex = 16;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(496, 58);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "SEARCH PRODUCT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxProductSearch);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridViewInflation);
+            this.Controls.Add(this.textBoxUnit);
+            this.Controls.Add(this.textBoxMarket);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxQuantity);
+            this.Controls.Add(this.textBoxProduct);
+            this.Controls.Add(this.textBoxCode);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInflation)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxCode;
+        private System.Windows.Forms.TextBox textBoxProduct;
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxMarket;
+        private System.Windows.Forms.TextBox textBoxUnit;
+        private System.Windows.Forms.DataGridView dataGridViewInflation;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxProductSearch;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ EN/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Contexts;
+
+namespace EN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection ("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=emflasyon;Integrated Security=True");
+
+        public void dataGet(string DataList)
+        { 
+            SqlDataAdapter adapter = new SqlDataAdapter(DataList, conn);
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            dataGridViewInflation.DataSource = dataSet.Tables[0];
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGet("SELECT * FROM Table_1");
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand addCommand = new SqlCommand();
+            addCommand.Connection = conn;
+            addCommand.CommandText = "INSERT INTO TABLE_1 (CODE, PRODUCT, QUANTITY, UNIT, PRICE, MARKET) VALUES (@CODES, @PRODUCTS, @QUANTITYS, @UNITS, @PRICES, @MARKETS)";
+            addCommand.Parameters.AddWithValue("@CODES", textBoxCode.Text);
+            addCommand.Parameters.AddWithValue("@PRODUCTS", textBoxProduct.Text);
+            addCommand.Parameters.AddWithValue("@QUANTITYS", textBoxQuantity.Text);
+            addCommand.Parameters.AddWithValue("@UNITS", textBoxUnit.Text);
+            addCommand.Parameters.AddWithValue("@PRICES", textBoxPrice.Text);
+            addCommand.Parameters.AddWithValue("@MARKETS", textBoxMarket.Text);
+            addCommand.ExecuteNonQuery();
+            dataGet("SELECT * FROM Table_1");
+            conn.Close();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand commandSearch = new SqlCommand();
+            commandSearch.Connection = conn;
+            commandSearch.CommandText = ("SELECT * FROM Table_1 WHERE PRODUCT LIKE '%" + textBoxProductSearch.Text + "%'    ");
+            SqlDataAdapter adapterSearch = new SqlDataAdapter(commandSearch);
+            DataSet dataSet = new DataSet();
+            adapterSearch.Fill(dataSet);
+            dataGridViewInflation.DataSource = dataSet.Tables[0];
+            conn.Close();
+        }
+    }
+}
